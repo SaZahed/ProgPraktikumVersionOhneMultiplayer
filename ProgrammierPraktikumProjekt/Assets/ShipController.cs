c0957de05b7b45613bef1c49d1ad55f4f8a0fc8b
SarahBibi7 <szahed@stud.hs-bremen.de>
2025-05-14 21:35:22 +0200

    Code letzter Wocher
    Ich habe fast den ganzen Code des labors von letzer woche eingefügt. Die Diane rotiert verrückt --> es ist nichts vollständiges

---
+++ ProgrammierPraktikumProjekt/Assets/ShipController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private float thrustForce = 500f; // schubkraft motor
+    [SerializeField] private float dragCoefficient = 0.1f; // widerstandskoeffizient
+    [SerializeField] private float rotationSpeed = 50f; // drehgeschgeschwindigkeit
+
+    private Rigidbody rb;   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>(); // Rigidbody-Referenz abrufen   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float thrustInput = Input.GetAxis("Vertical"); // w/s schubsteuerung
+        float turnInput = Input.GetAxis("Horizontal"); // a/d drehsteuerung
+
+        applyForces(thrustInput);
+        appplyRoatation(turnInput);
+    }
+    private void appplyRoatation(float turnInput)
+    {
+     if(Mathf.Abs(turnInput)> 0.1f)
+        {
+            float adjustTurnSpeed = rotationSpeed * (rb.linearVelocity.magnitude / 10f);
+            float turnTorque = turnInput * adjustTurnSpeed;
+
+            rb.AddTorque(Vector3.up * turnTorque, ForceMode.Acceleration);
+        }  
+    }
+    private void applyForces(float thrustInput)
+
+    {
+        //Kräfte berechnen
+        float thrust = thrustInput * thrustForce;
+        //float drag = dragCoefficient * rb.maxLinearVelocity.magnitude * rb.linearVelocity.magnitude;
+
+        //Nettokraft berechnen 
+
+        //Kraft auf das Rigidbody anwenden
+    }
+}
