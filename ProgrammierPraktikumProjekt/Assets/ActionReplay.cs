65f57db40ab7f81976efa54032d14aeeead9e022
SarahBibi7 <szahed@stud.hs-bremen.de>
2025-06-28 12:23:21 +0200

    Replay Modus - Ausbaufähig
    - man muss das Skript ActionReplay im Inspector von BS Diane ranhängen
    - mithilfe von einem Tutorial begonnen, aber dann Probleme mit Rigidbody, die ich durch chatgpt anpassen konnte:
    --> Die rigidbody-Variable wurde vorher lokal in Start() überschrieben. Jetzt korrekt: this.rigidbody = GetComponent<Rigidbody>();
    
    -->Die Aufzeichnung verwendet jetzt ein separates Objekt (record) pro Frame, sauber gespeichert und wieder abspielbar.
    
    Leider wird UI nicht wirklich angepasst und die Kameraperspektive nicht angepasst

---
+++ ProgrammierPraktikumProjekt/Assets/ActionReplay.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ActionReplay : MonoBehaviour
+{
+    private bool isInReplayMode = false;
+    private Rigidbody rb;
+    private List<ActionReplayRecords> records = new List<ActionReplayRecords>();
+    private int replayIndex = 0; //angepasst mit ChatGpt
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isInReplayMode = !isInReplayMode;
+            replayIndex = 0;
+
+            if (isInReplayMode)
+            {
+                rb.isKinematic = true;
+            }
+            else
+            {
+                rb.isKinematic = false;
+                records.Clear();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isInReplayMode)
+        {
+            if (replayIndex < records.Count)
+            {
+                SetTransform(replayIndex);
+                replayIndex++;
+            }
+        }
+        else
+        {
+            records.Add(new ActionReplayRecords
+            {
+                position = transform.position,
+                rotation = transform.rotation,
+            });
+        }
+    }
+
+    private void SetTransform(int index)
+    {
+        var record = records[index];
+        transform.position = record.position;
+        transform.rotation = record.rotation;
+    }
+}
---
+++ ProgrammierPraktikumProjekt/Assets/ActionReplayRecords.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class ActionReplayRecords
+{
+    public Vector3 position;
+
+    public Quaternion rotation; //weil auch rotation aufgezeichnet werden soll
+
+    
+}
